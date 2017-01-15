ca139bcc405fcf8654315e069f480ed51eed8d42
Mariusz Osiecki <avalanchy@gmail.com>
2017-01-15 19:09:08 +0100

    alpha of searching and playing

--- addon.py
+++ addon.py
@@ -18,18 +18,15 @@ __handle__ = int(sys.argv[1])
 __query__ = sys.argv[2]
 
 
-
-def cda_id_to_object(url='http://m.cda.pl/video/11557750e?wersja=720p'):
+def get_cda_video_data(cda_id, quality):
+    url = 'http://m.cda.pl/video/{}?wersja={}'
+    url = url.format(cda_id, quality)
+    logging.warn(url)
     response = urllib2.urlopen(url)
     html = response.read()
     title = re.search('<title>([^<]+)', html).group(1)
     player_data = re.search("player_data='([^']+)", html).group(1)
     player_data = json.loads(player_data)
-    return {
-        'title': "Kevinn",
-        'poster_url': '',
-        'video_url': '',
-    }
     return {
         'title': title,
         'poster_url': player_data['video']['poster'],
@@ -37,9 +34,10 @@ def cda_id_to_object(url='http://m.cda.pl/video/11557750e?wersja=720p'):
     }
 
 
-
+logging.warn(sys.argv)
 
 import urllib
+from bs4 import BeautifulSoup
 
 def request_search(query):
     query = urllib.quote_plus(query)
@@ -48,51 +46,65 @@ def request_search(query):
     logging.warn(url)
     response = urllib2.urlopen(url)
     html = response.read()
-    titles = re.findall('<h2 class="title">([^<]+)', html)
-    thumbs = re.findall('<img class="box-elem-img" src="([^"]+)', html)
-    cda_ids = re.findall('<a class="link-box-elem" href="/video/([^"]+)', html)
-    return zip(titles, thumbs, cda_ids)
+    items = []
+    bs = BeautifulSoup(html)
+    results = bs.find_all('div', attrs={'class': 'box-elem'})
+    for result in results:
+        cda_id = result.find('a').attrs['href'].split('/')[-1]
+        title = result.find('h2').text
+        thumb = result.find('img').attrs['src']
+        quality = result.find('span', attrs={'class': 'quality'})
+        quality = quality.text if quality else ''
+        items.append((cda_id, title, thumb, quality))
+    return items
+
+
+match = re.match('\?cda-id=(\w+)&quality=(\w*)', __query__)
 
 
 
+if match:
+    cda_id, quality = match.groups()
+    obj = get_cda_video_data(cda_id, quality)
 
-
-if __query__.startswith('?cda-id='):
     # we gonna play movie
+    player = xbmc.Player()
+    player.play(obj['video_url'])
+
     pass
-    logging.warn('plaing movie')
-else:
-    logging.warning(sys.argv)
+    logging.warn(obj)
+    sys.exit(0)
 
-    addon = xbmcaddon.Addon()
-    addonname = addon.getAddonInfo('name')
+logging.warning(sys.argv)
 
-    logging.warn('before modal')
-    keyboard = xbmc.Keyboard('', 'Wpisz tytuł filmu', False)
-    keyboard.doModal()
-    logging.warn('after modal')
-    user_input = keyboard.getText()
-    if keyboard.isConfirmed() and user_input != '':
-        search_results = request_search(user_input)
-        logging.warn(search_results)
+addon = xbmcaddon.Addon()
+addonname = addon.getAddonInfo('name')
 
-        kodi_search_res = []
+logging.warn('before modal')
+keyboard = xbmc.Keyboard('', 'Wpisz tytuł filmu', False)
+keyboard.doModal()
+logging.warn('after modal')
+user_input = keyboard.getText()
+if keyboard.isConfirmed() and user_input != '':
+    search_results = request_search(user_input)
+    logging.warn(search_results)
 
-        for title, thumb, cda_id in search_results:
-            url = '{}?cda-id={}'.format(__url__, cda_id)
-            list_item = xbmcgui.ListItem(label=title, thumbnailImage=thumb)
-            list_item.setInfo('video', {})
-            kodi_search_res.append((url, list_item, False))
+    kodi_search_res = []
 
-        xbmcplugin.addDirectoryItems(__handle__, kodi_search_res)
+    for cda_id, title, thumb, quality in search_results:
+        url = '{}?cda-id={}&quality={}'.format(__url__, cda_id, quality)
+        list_item = xbmcgui.ListItem(label=title, thumbnailImage=thumb)
+        list_item.setInfo('video', {})
+        kodi_search_res.append((url, list_item, False))
+
+    xbmcplugin.addDirectoryItems(__handle__, kodi_search_res)
+
+search_item = xbmcgui.ListItem(label='Search...')
+xbmcplugin.addDirectoryItem(handle=__handle__, url=__url__+ '?mode=search', listitem=search_item, isFolder=False, totalItems=1)
 
 
-    search_item = xbmcgui.ListItem(label='Search...')
-    xbmcplugin.addDirectoryItem(handle=__handle__, url=__url__+ '?mode=search', listitem=search_item, isFolder=False, totalItems=1)
-
-
-    xbmcplugin.addSortMethod(__handle__, xbmcplugin.SORT_METHOD_NONE)
-    xbmcplugin.endOfDirectory(__handle__)
+xbmcplugin.addSortMethod(__handle__, xbmcplugin.SORT_METHOD_NONE)
+xbmcplugin.endOfDirectory(__handle__)
 
 
     #    line1 = "Hello World!"
