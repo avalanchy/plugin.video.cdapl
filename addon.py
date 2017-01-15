f6aa930db6db7c35d78a2b9c83b5fe6d2a9480d6
Mariusz Osiecki <avalanchy@gmail.com>
2017-01-15 20:43:47 +0100

    add video duration

--- addon.py
+++ addon.py
@@ -47,7 +47,7 @@ def request_search(query):
     response = urllib2.urlopen(url)
     html = response.read()
     items = []
-    bs = BeautifulSoup(html)
+    bs = BeautifulSoup(html, 'lxml')
     results = bs.find_all('div', attrs={'class': 'box-elem'})
     for result in results:
         cda_id = result.find('a').attrs['href'].split('/')[-1]
@@ -55,7 +55,10 @@ def request_search(query):
         thumb = result.find('img').attrs['src']
         quality = result.find('span', attrs={'class': 'quality'})
         quality = quality.text if quality else ''
-        items.append((cda_id, title, thumb, quality))
+        duration = result.find('span', attrs={'class': 'timeElem'}).text
+        h, m, s = duration.split(':')
+        duration = int(h) * 3600 + int(m) * 60 + int(s)
+        items.append((cda_id, title, thumb, quality, duration))
     return items
 
 
@@ -70,8 +73,6 @@ if match:
     # we gonna play movie
     player = xbmc.Player()
     player.play(obj['video_url'])
-
-    pass
     logging.warn(obj)
     sys.exit(0)
 
@@ -91,10 +92,12 @@ if keyboard.isConfirmed() and user_input != '':
 
     kodi_search_res = []
 
-    for cda_id, title, thumb, quality in search_results:
+    for cda_id, title, thumb, quality, duration in search_results:
         url = '{}?cda-id={}&quality={}'.format(__url__, cda_id, quality)
         list_item = xbmcgui.ListItem(label=title, thumbnailImage=thumb)
         list_item.setInfo('video', {})
+        height = re.search('\d*', quality).group()
+        list_item.addStreamInfo('video', {'duration': duration, 'height': height})
         kodi_search_res.append((url, list_item, False))
 
     xbmcplugin.addDirectoryItems(__handle__, kodi_search_res)
@@ -105,11 +108,3 @@ xbmcplugin.addDirectoryItem(handle=__handle__, url=__url__+ '?mode=search', list
 
 xbmcplugin.addSortMethod(__handle__, xbmcplugin.SORT_METHOD_NONE)
 xbmcplugin.endOfDirectory(__handle__)
-
-
-    #    line1 = "Hello World!"
-#    line2 = "Nice to meet you %s" %(keyboard.getText())
-#    line3 = "Welcome to Kodi!"
-#
-#    xbmcgui.Dialog().ok(addonname, line1, line2, line3) ###
-
