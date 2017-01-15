e70b2eb58b874ce02d22188c29d5b80167b056f4
Mariusz Osiecki <avalanchy@gmail.com>
2017-01-15 22:26:48 +0100

    trying to display hd icon

--- addon.py
+++ addon.py
@@ -21,7 +21,6 @@ __query__ = sys.argv[2]
 def get_cda_video_data(cda_id, quality):
     url = 'http://m.cda.pl/video/{}?wersja={}'
     url = url.format(cda_id, quality)
-    logging.warn(url)
     response = urllib2.urlopen(url)
     html = response.read()
     title = re.search('<title>([^<]+)', html).group(1)
@@ -34,7 +33,6 @@ def get_cda_video_data(cda_id, quality):
     }
 
 
-logging.warn(sys.argv)
 
 import urllib
 from bs4 import BeautifulSoup
@@ -43,7 +41,6 @@ def request_search(query):
     query = urllib.quote_plus(query)
     url_base = 'http://m.cda.pl/szukaj?q={}&gdzie=v&duration=dlugie&quality=all&s=best'
     url = url_base.format(query)
-    logging.warn(url)
     response = urllib2.urlopen(url)
     html = response.read()
     items = []
@@ -73,31 +70,26 @@ if match:
     # we gonna play movie
     player = xbmc.Player()
     player.play(obj['video_url'])
-    logging.warn(obj)
     sys.exit(0)
 
-logging.warning(sys.argv)
 
 addon = xbmcaddon.Addon()
 addonname = addon.getAddonInfo('name')
 
-logging.warn('before modal')
-keyboard = xbmc.Keyboard('', 'Wpisz tytuł filmu', False)
-keyboard.doModal()
-logging.warn('after modal')
-user_input = keyboard.getText()
-if keyboard.isConfirmed() and user_input != '':
+user_input = xbmcgui.Dialog().input('Wpisz tytuł filmu')
+if user_input != '':
     search_results = request_search(user_input)
-    logging.warn(search_results)
 
     kodi_search_res = []
 
     for cda_id, title, thumb, quality, duration in search_results:
         url = '{}?cda-id={}&quality={}'.format(__url__, cda_id, quality)
         list_item = xbmcgui.ListItem(label=title, thumbnailImage=thumb)
-        list_item.setInfo('video', {})
+        list_item.setInfo('video', {'plot': 'bla bla bla', 'mediatype': 'movie', 'date': '01.01.2009'})
+        list_item.setProperty("IsPlayable", "true")
         height = re.search('\d*', quality).group()
-        list_item.addStreamInfo('video', {'duration': duration, 'height': height})
+        list_item.addStreamInfo('video', {'codec': 'h264', 'aspect': 1.78, 'width': 1920, 'height': 1080, 'duration': duration})
+        list_item.setArt({'icon': 'icon.png'})
         kodi_search_res.append((url, list_item, False))
 
     xbmcplugin.addDirectoryItems(__handle__, kodi_search_res)
